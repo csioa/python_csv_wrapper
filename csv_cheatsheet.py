60a0bf1c10568b6ac14ebc87948225feb74d1d05
csioa <babispasg@gmail.com>
2019-06-16 10:57:25 +0200

    New file_to_list converts to list of lists, and check for file existence

--- csv_cheatsheet.py
+++ csv_cheatsheet.py
@@ -1,21 +1,26 @@
 import csv
+import os
 from collections import namedtuple
 
 
 class FileConverter:
 
-    def __init__(self, file_path):
+    def __init__(self, file_path=""):
         """
         :param file_path: the path to access the CSV file that the FileConverter should covnert
         :type file_path: string
         """
         self._file_path = file_path
+        self._file_exists = os.path.isfile(file_path)
 
-        with open(self._file_path, 'r') as filename:
-            reader = csv.reader(filename)
-            self._Row = namedtuple('Row', ' '.join(next(reader, None)))
+        if self._file_exists:
+            with open(self._file_path, 'r') as filename:
+                reader = csv.reader(filename)
+                self._Row = namedtuple('Row', ' '.join(next(reader, None)))
 
-        self._headers = list(self._Row._fields)
+            self._headers = list(self._Row._fields)
+        else:
+            print("\n File not found!")
 
     def get_filename(self):
         return self._file_path
@@ -24,7 +29,10 @@ class FileConverter:
         """
         :return: list of headers from the given CSV file
         """
-        return self._headers
+        headers = []
+        if self._file_exists:
+            headers = self._headers
+        return headers
 
     def file_to_namedtuple(self, with_headers=True):
         """
@@ -32,12 +40,15 @@ class FileConverter:
         :type with_headers: boolean
         :return: list of Rows - every Row wraps on row of the CSV file
         """
-        with open(self._file_path, 'r') as filename:
-            reader = csv.reader(filename)
-            if not with_headers:
-                next(reader, None)
+        namedtuple_list = []
+        if self._file_exists:
+            with open(self._file_path, 'r') as filename:
+                reader = csv.reader(filename)
+                if not with_headers:
+                    next(reader, None)
 
-            return map(self._Row._make, reader)
+                namedtuple_list = map(self._Row._make, reader)
+        return namedtuple_list
 
     # def write_new_csv(self, file_path, headers, data):
 
@@ -47,23 +58,37 @@ class FileConverter:
         together with the respective keys
         """
         csv_dict_list = []
-
-        with open(self._file_path, 'r') as filename:
-            reader = csv.reader(filename)
-            next(reader, None)
-            for row in reader:
-                csv_dict_list.append(dict(zip(self._headers, row)))
+        if self._file_exists:
+            with open(self._file_path, 'r') as filename:
+                reader = csv.reader(filename)
+                next(reader, None)
+                for reader_row in reader:
+                    csv_dict_list.append(dict(zip(self._headers, reader_row)))
 
         return csv_dict_list
 
-    # def get_list(self):
+    def file_to_list(self, with_headers=True):
+        """
+        :param with_headers: true if the returned iterable should contain the heards - false otherwise
+        :type with_headers: boolean
+        :return:
+        """
+        list_of_lists = []
+        if self._file_exists:
+            with open(self._file_path, 'r') as filename:
+                reader = csv.reader(filename)
+                if not with_headers:
+                    next(reader, None)
+                for reader_row in reader:
+                    list_of_lists.append(reader_row)
+        return list_of_lists
 
 
 if __name__ == '__main__':
 
-    fr = FileConverter("csv_files/mock_data.csv")
+    fr = FileConverter("csv_files/payment_events_mock.csv")
+    # fr = FileConverter("csv_files/customer_profile_mock.csv")
 
-    # Getting a list of the headers of the file
     print ("==========================================================================================="
            "===============================")
     print ("\n Reading CSV file {file}".format(file=fr.get_filename()))
@@ -74,13 +99,18 @@ if __name__ == '__main__':
     print ("\n========================================================================================="
            "=================================")
     print (" \n Sample output for CSV to namedtuple - first 5 elements\n")
-    for r in fr.file_to_namedtuple(False)[:5]:
-        print "{fn} {ln} with ID = {id}, lives in {city}, {country}"\
-            .format(fn=r.first_name, ln=r.last_name, id=r.id, city=r.city, country=r.country)
+    for row in fr.file_to_namedtuple(False)[:5]:
+        print row
     print ("\n========================================================================================="
            "=================================")
     print (" \n Sample output for CSV to list of dictionaries - 1 element\n")
-    for field, value in fr.file_to_dict_list()[0].items():
-        print ("{field} : {value}".format(field=field, value=value))
+    for row in fr.file_to_dict_list()[:1]:
+        for field, value in row.items():
+            print ("{field} : {value}".format(field=field, value=value))
+    print ("\n========================================================================================="
+           "=================================")
+    print ("\n Sample output for CSV to list of lists - 5 elements\n")
+    for row in fr.file_to_list(False)[:5]:
+        print row
     print ("\n========================================================================================="
            "=================================")
